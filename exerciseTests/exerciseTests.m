37352950010d7f432ca56dff48169444859e8fca
Yoshiya Hinosawa <stibium121@gmail.com>
2014-02-07 13:31:28 +0900

    Add sample tests

--- exerciseTests/exerciseTests.m
+++ exerciseTests/exerciseTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "KT3WebViewDelegate.h"
 
 @interface exerciseTests : XCTestCase
 
@@ -17,18 +18,21 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testExample
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTAssertTrue(YES, @"test");
+}
+
+- (void)testDelegateStart
+{
+    XCTAssertTrue([[[KT3WebViewDelegate alloc] init] webView:nil shouldStartLoadWithRequest:nil navigationType:nil]);
 }
 
 @end
