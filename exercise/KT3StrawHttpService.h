fdb168f81053d9966a941bc1106fe88f58824b93
Yoshiya Hinosawa <stibium121@gmail.com>
2014-07-05 23:36:54 +0100

    Delete obsolete KT3StrawHttpService

--- exercise/KT3StrawHttpService.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Straw/Straw.h>
-
-@interface KT3StrawHttpService : NSObject <STWService>
-
-- (void)method:(NSDictionary *)params withContext:(id<STWServiceCallContext>)context;
-
-@end
